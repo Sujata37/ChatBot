76d98282ec7f9337919291d71540b23d1ee0779e
Sujata37 <sujatakandel7@gmail.com>
2026-01-11 20:42:32 +0100

    forth day

---
+++ script.js
@@ -0,0 +1,28 @@
+const chatBody = document.querySelector(".chat-body");
+
+const messageInput = document.querySelector(".message-input");
+
+// create message element with dynamic classes and return it
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+
+// Handle outgoing user message
+const handleOutgoingMessage = (userMessage) => {
+
+// Create and display user message
+    const messageContent = `<div class="message-text">${userMessage}</div>`;
+    const outgoingMessageDiv =createMessageElement(messageContent, "user-message");
+    chatBody.appendChild(outgoingMessageDiv);
+}
+
+
+messageInput.addEventListener("keydown", (e) => {
+    const userMessage = e.target.value.trim();
+    if(e.key === "Enter" && userMessage) {
+        handleOutgoingMessage(userMessage);
+    }
+});
