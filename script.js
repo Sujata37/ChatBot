4a7048d34cb2267f952afabf475c526dc275c113
Sujata37 <sujatakandel7@gmail.com>
2026-01-24 12:51:58 +0100

    change

--- script.js
+++ script.js
@@ -163,7 +163,19 @@ fileCancelButton.addEventListener("click", () => {
 const picker = new EmojiMart.Picker({
     theme : "light",
     skinTonePosition : "none",
-    previewPosition : "none"
+    previewPosition : "none",
+    onEmojiSelect: (emoji) => {
+        const { selectionStart: start, selectionEnd: end } = messageInput;
+        messageInput.setRangeText(emoji.native, start,end, "end");
+        messsageInput.focus();
+    },
+    onClickOutside: (e) => {
+        if(e.target.id === "emoji-picker"){
+            document.body.classList.toggle("show-emoji-picker");
+        } else{
+            document.body.classList.remove("show-emoji-picker");
+        }
+    }
 });
 
 document.querySelector(".chat-form").appendChild(picker);
